4466b9d4a49557b074d1d13ba4303d40357f307d
kristopherbullinger <kris.bullinger@gmx.com>
2018-12-07 11:31:48 -0500

    lab complete

--- src/reducers/manageRestaurants.js
+++ src/reducers/manageRestaurants.js
@@ -2,15 +2,13 @@ export default function manageRestaurants(state = {
   restaurants: []
 }, action) {
   switch (action.type) {
-
     case 'ADD_RESTAURANT':
+        debugger
       return {
         ...state,
         restaurants: [...state.restaurants, action.restaurant]
       }
-
     default:
       return state;
-
   }
 };
